436e2bf9d2e17ab7a1a141fbae399f2eaf960620
tomama <nabilatamimi09@gmail.com>
2017-03-16 22:40:17 +0700

    Rental Motor

---
+++ src/uts_pbo/KoneksiDB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_pbo;
+
+import com.mysql.jdbc.Connection; 
+import java.sql.DriverManager; 
+import java.sql.ResultSet; 
+import java.sql.SQLException; 
+import java.sql.Statement; 
+import java.util.logging.Level; 
+import java.util.logging.Logger; 
+
+/**
+ *
+ * @author tomama
+ */
+public class KoneksiDB {
+
+    static int execute(String SQL) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ src/uts_pbo/UTS_PBO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_pbo;
+
+/**
+ *
+ * @author tomama
+ */
+public class UTS_PBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
